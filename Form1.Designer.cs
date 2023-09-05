8bf147eb51d08e3d8d7b6edd5a45004db0df4e6f
vascodes <nikhilvas12@gmail.com>
2023-09-05 07:45:56 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FootballBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.richTextBoxChat = new System.Windows.Forms.RichTextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Location = new System.Drawing.Point(10, 346);
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.Size = new System.Drawing.Size(778, 20);
+            this.textBoxChat.TabIndex = 0;
+            this.textBoxChat.Text = "Type your response here";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(4, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(175, 36);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Football Bot";
+            // 
+            // richTextBoxChat
+            // 
+            this.richTextBoxChat.Location = new System.Drawing.Point(10, 48);
+            this.richTextBoxChat.Name = "richTextBoxChat";
+            this.richTextBoxChat.Size = new System.Drawing.Size(778, 268);
+            this.richTextBoxChat.TabIndex = 2;
+            this.richTextBoxChat.Text = "Welcome to the Football Chatbot!";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(713, 395);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.richTextBoxChat);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.textBoxChat);
+            this.Name = "Form1";
+            this.Text = "Football Bot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxChat;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.RichTextBox richTextBoxChat;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
